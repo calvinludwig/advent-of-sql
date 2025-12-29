a42f70733fbdbfb0b8c31327d399a2bf8617b9a5
Calvin Ludwig <31071765+calvinludwig@users.noreply.github.com>
2025-12-29 20:14:11 -0300

    day 4

---
+++ 2025/day4.sql
@@ -0,0 +1,21 @@
+-- my solution
+create extension if not exists pg_trgm;
+
+select last_minute_signups.volunteer_name,
+       ( select val
+         from unnest(array [ 'Stage Setup', 'Cocoa Station', 'Parking Support', 'Choir Assistant', 'Snow Shoveling', 'Handwarmer Handout']) as val
+         order by assigned_task <-> val
+         limit 1 )                                        as role,
+       case when time_slot = 'noon' then '12:00 PM'
+            when time_slot like '2%' then '2:00 PM'
+            when time_slot like '10%' then '10:00 AM' end as shift_time
+from last_minute_signups
+union
+select volunteer_name,
+       ( select val
+         from unnest(array [ 'Stage Setup', 'Cocoa Station', 'Parking Support', 'Choir Assistant', 'Snow Shoveling', 'Handwarmer Handout']) as val
+         order by role <-> val
+         limit 1 ) as role,
+       shift_time
+from official_shifts
+order by 1 asc;
