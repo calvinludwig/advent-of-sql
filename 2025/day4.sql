d079338291e04a0781ee9bc5aef9ac5341be3556
Calvin Ludwig <31071765+calvinludwig@users.noreply.github.com>
2025-12-30 09:48:04 -0300

    feat: better solution for day 4

--- 2025/day4.sql
+++ 2025/day4.sql
@@ -19,3 +19,24 @@ select volunteer_name,
        shift_time
 from official_shifts
 order by 1 asc;
+
+-- a better solution that I came up after day 5 learnings
+with roles(r) as
+         ( values ('Stage Setup'),
+                  ('Cocoa Station'),
+                  ('Parking Support'),
+                  ('Choir Assistant'),
+                  ('Snow Shoveling'),
+                  ('Handwarmer Handout') )
+select last_minute_signups.volunteer_name,
+       ( select r from roles order by assigned_task <-> r limit 1 ) as role,
+       case when time_slot = 'noon' then '12:00 PM'
+            when time_slot like '2%' then '2:00 PM'
+            when time_slot like '10%' then '10:00 AM' end           as shift_time
+from last_minute_signups
+union
+select volunteer_name,
+       ( select r from roles order by role <-> r limit 1 ) as role,
+       shift_time
+from official_shifts
+order by 1 asc;
--- 2025/day5.sql
+++ 2025/day5.sql
@@ -27,16 +27,17 @@ where rank <= 3;
 -- my solution was pretty close to aaron's, but I didn't know that I could order by count(*) inside row number partition
 
 -- aaron's solution with cte, more readable
-with ranked as ( select user_name,
-                        artist,
-                        count(*) as play_count,
-                        row_number() over (
-                            partition by user_name
-                            order by count(*) desc, artist asc
-                            )    as rank
-                 from listening_logs
-                 group by 1, 2
-                 order by 1, 3 desc )
+with ranked as
+         ( select user_name,
+                  artist,
+                  count(*) as play_count,
+                  row_number() over (
+                      partition by user_name
+                      order by count(*) desc, artist asc
+                      )    as rank
+           from listening_logs
+           group by 1, 2
+           order by 1, 3 desc )
 select *
 from ranked
 where rank <= 3;
